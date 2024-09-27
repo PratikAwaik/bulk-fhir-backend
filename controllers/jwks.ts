b22f915185b95a1bec8996dbe0356650a88738f2
PratikAwaik <pratikawaik125@gmail.com>
2024-09-28 03:46:00 +0530

    add jwks and add node_modues in gitignore

---
+++ controllers/jwks.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import fs from "fs";
+import jose from "node-jose";
+
+const ks = fs.readFileSync("keys.json");
+
+export const serveJWKS = async (req: Request, res: Response) => {
+  const keyStore = await jose.JWK.asKeyStore(ks.toString());
+  res.json(keyStore.toJSON());
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("TS");
+import express from "express";
+
+// controllers
+import * as jwksController from "./controllers/jwks";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/jwks", jwksController.serveJWKS);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
