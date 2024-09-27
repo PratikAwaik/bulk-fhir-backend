8131541a09fb117a3cdf81ca29240c23a8d49c40
PratikAwaik <pratikawaik125@gmail.com>
2024-09-28 03:31:37 +0530

    init setup and add generate keys

---
+++ scripts/generate-keys.ts
@@ -0,0 +1,11 @@
+import jose from "node-jose";
+import fs from "fs";
+
+const keyStore = jose.JWK.createKeyStore();
+
+keyStore.generate("RSA", 2048, { alg: "RS348", use: "sig" }).then((result) => {
+  fs.writeFileSync(
+    "keys.json",
+    JSON.stringify(keyStore.toJSON(true), null, "   ")
+  );
+});
