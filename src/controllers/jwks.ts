d3e1a6d431b93a7f40b6ed7278c7cabac038eecd
PratikAwaik <pratikawaik125@gmail.com>
2024-09-29 03:38:27 +0530

    put readFile in serveJWKS

--- src/controllers/jwks.ts
+++ src/controllers/jwks.ts
@@ -2,9 +2,8 @@ import { Request, Response } from "express";
 import fs from "fs";
 import jose from "node-jose";
 
-const ks = fs.readFileSync("keys.json");
-
 export const serveJWKS = async (_: Request, res: Response) => {
+  const ks = fs.readFileSync("keys.json");
   const keyStore = await jose.JWK.asKeyStore(ks.toString());
   res.json(keyStore.toJSON());
 };
